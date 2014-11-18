2a11e4e5e3e90f66d8437e89c8378ea26b0e0ef4
John <john@yoga>
2014-11-18 11:31:26 +0300

    Attempt to enforce priority by parentheses

--- src/calclj/core.clj
+++ src/calclj/core.clj
@@ -16,36 +16,59 @@
     ))
 
 
-(defn ^:dynamic result [p left]
-  (assoc p :res left))
+(defn next-token [e]
+  (let [r (rest e)
+        t (first e)
+        ee (cond
+            (sequential? t)
+            (concat ["("] t [")"] r)
+            :else e
+            )]
+    {:token (first ee)
+     :exp (rest ee)}
+    )
+  )
+
+(next-token [1 "+" 2])
+(next-token [[1 "+" 2] "/" 3])
+
+(defn ^:dynamic advance [p]
+  (merge p (next-token (:exp p))))
+
+(defn )
+
+(defn ^:dynamic result [p v] (assoc p :res v))
 
 (defn ^:dynamic equal-priority [left token rbp p]
   (if token
     (if (> (lbp token) rbp)
       (let [l (led token left p)
+            n (advance l)
             e (:exp l)]
         (equal-priority
-         (:res l) (first e) rbp
-         (assoc p :exp (rest e)))
-       )
-      {:res left :exp (cons token (:exp p))})
-    (assoc p :res left))
+         (:res l) (:token n) rbp n)
+        )
+      (result p left)
+      ; {:res left :exp (cons token (:exp p))}
+      )
+    (result p left)
+    )
   )
 
 (defn ^:dynamic expression [rbp p]
-  (if-let [left (-> p :exp first)]
-    (if-let [r (-> p :exp rest)]
-      (equal-priority left (first r) rbp
-                      (assoc p :exp (rest r)))
-      {:res left :exp []})
-    {:res 0 :exp []})
+  (let [n (advance p)
+        nn (advance n)]
+    (if-let [left (:token n)]
+      (if-let [token (:token nn)]
+        (equal-priority left token rbp nn)
+        (result n left))
+      (result n 0))
+    )
   )
 
 (defn ^:dynamic led [token left p]
   (let [r (expression (lbp token) p)]
-    (assoc r :res
-      [token left (:res r)]
-      )
+    (result r [token left (:res r)])
     )
   )
 
@@ -55,15 +78,19 @@
    0 {:exp [1 "+" 2]
       :res []}))
 
+(clojure.tools.trace/dotrace
+ [expression equal-priority led]
+ (expression 0
+             {:exp [1 "+" 2 "*" 5 "+" 4]
+              :res []}))
+
 (def p
   (clojure.tools.trace/dotrace
      [expression equal-priority led]
      (expression 0
-                 {:exp [1 "+" 2 "*" 5 "+" 4]
+                 {:exp [[1 "+" 2] "/" 3]
                   :res []}))
   )
-
-;(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
 (defn -main
   "I don't do a whole lot ... yet."
